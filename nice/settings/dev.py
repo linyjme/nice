ea9d0cd91cb0a27fb7930b0e3f04405461f8b3cb
linyj <linyj_work@163.com>
2021-03-04 00:15:38 +0800

    add mysql

--- nice/settings/dev.py
+++ nice/settings/dev.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'nice.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nice',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':3306,
+    },
 }
 
 # Password validation
